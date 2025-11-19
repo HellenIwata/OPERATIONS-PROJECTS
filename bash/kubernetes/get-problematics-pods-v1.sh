8062a357cbc27d7b35ea1dd25153584e1da4607a
HellenIwata <hellen.iwata001@gmail.com>
2025-11-19 03:46:09 -0300

    FEAT (get-problematics-pod): add script to identify problematic pods in Kubernetes and output results in JSON format

---
+++ bash/kubernetes/get-problematics-pods-v1.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+# ==============================================================================
+# Author:         Hellen Iwata
+# Create date:    2025-09-30
+# Version:        1.0.0
+# Description:    Este script varre todos os namespaces de um cluster Kubernetes
+#                 em busca de pods com status problemáticos (como Crash, Evicted,
+#                 Error, ContainerCreating). As informações de namespace e nome
+#                 dos pods encontrados são salvas em um arquivo de saída no
+#                 formato JSON.
+#
+# Dependencies:   - kubectl (configurado e com acesso ao cluster)
+#
+# Output Format:  O script gera um arquivo JSON contendo uma lista de objetos,
+#                 cada um com o namespace e o nome do pod problemático.
+#
+# Usage:          ./get-problematics-pod.sh
+# ==============================================================================
+
+# VARIABLES
+timestamp=$(date +%Y-%m-%d_%H-%M-%S)
+output_file="problematic_pods_$timestamp.json"
+
+log "INFO" "Buscando pods com status problemáticos em todos os namespaces..."
+
+# Faz uma única chamada para obter todos os pods problemáticos e formata a saída para JSON com jq
+# Isso é mais eficiente do que iterar sobre cada namespace.
+kubectl get pods --all-namespaces -o json | jq -r '
+  .items[] | 
+  select(.status.phase | test("Failed|Unknown")) or 
+  (.status.containerStatuses[]? | select(.state.waiting.reason == "CrashLoopBackOff" or .state.terminated.reason == "Error")) |
+  {namespace: .metadata.namespace, pod_name: .metadata.name}
+' | jq -s '.' > "$output_file"
+
+for ns in $namespace; do
+	pods=$(kubectl get pods -n "$ns" --no-headers | grep -Ei "Crash|Evicted|Error|ContainerCreating" | awk  '{print $1}')
+	for pod in $pods; do
+		if [ "$firsts_entry" == true ]; then
+			firsts_entry=false
+		else
+			echo "," >> "$output_file"
+		fi
+		echo "{\"namespace\": \"$ns\", \"pod_name\": \"$pod\"}" >> "$output_file"
+	done
+done
+
+echo "]" >> "$output_file"
+
+echo "OUTPUT FILE: '$output_file'"
+log "SUCCESS" "Verificação concluída. Arquivo de saída gerado."
+log "INFO" "ARQUIVO DE SAÍDA: '$output_file'"
